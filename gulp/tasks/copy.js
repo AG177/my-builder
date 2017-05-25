74b66a9aa5db8bc59746365399aa02b57e3cea5a
Alexey Glushko <AlGlushko17@yandex.ru>
2017-05-25 14:17:13 +0300

    font and img

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,11 +2,13 @@
 
 module.exports = function() {
     $.gulp.task('gulp-copy', function () {
-        var sourceFiles = ['source/fonts/*', 'source/images/*.png'];
+        var sourceFiles = ['source/fonts/*', 'source/images/**/*.png'];
         var destination = './build/assets';
 
         return $.gulp
             .src(sourceFiles)
         .pipe($.gulpCopy(destination))
+
+
     });
 };
